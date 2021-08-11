6312db4a5fdf7cef413224077fda856bfbffde0f
Brian Walshe <walshe.brian@gmail.com>
2021-08-11 15:09:44 +0100

    Added parameter for exp moving average period

--- src/AggregateFunctions/AggregateFunctionExpMovingAverage.h
+++ src/AggregateFunctions/AggregateFunctionExpMovingAverage.h
@@ -12,14 +12,19 @@
 #include <DataTypes/DataTypesNumber.h>
 
 #include <AggregateFunctions/IAggregateFunction.h>
+#include <Common/FieldVisitorConvertToNumber.h>
 
-namespace ErrorCodes
-{
-    extern const int NOT_IMPLEMENTED;
-}
+
+
 
 namespace DB
 {
+
+namespace ErrorCodes
+{
+    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
+    extern const int NOT_IMPLEMENTED;
+}
 
 template <typename ValueType, typename TimestampType>
 struct AggregationFunctionExpMovingAverageData
@@ -29,6 +34,7 @@ struct AggregationFunctionExpMovingAverageData
     bool initialized = false;
 };
 
+
 template <typename ValueType, typename TimestampType>
 class AggregationFunctionExpMovingAverage final
     : public IAggregateFunctionDataHelper<
@@ -36,12 +42,27 @@ class AggregationFunctionExpMovingAverage final
         AggregationFunctionExpMovingAverage<ValueType, TimestampType>
       >
 {
+    const Float64 period;
+    
+    static Float64 getAveragingPeriod(const Array & params) 
+    {
+        if(params.empty())
+            return 1.0;
+
+        if(params.size() > 1)
+            throw Exception("One or zero parameter values expected for averaging period",
+                        ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+
+
+        return applyVisitor(FieldVisitorConvertToNumber<Float64>(), params[0]);
+    }
+
 public:
     AggregationFunctionExpMovingAverage(const DataTypes & arguments, const Array & params)
         : IAggregateFunctionDataHelper<
             AggregationFunctionExpMovingAverageData<ValueType, TimestampType>,
             AggregationFunctionExpMovingAverage<ValueType, TimestampType>
-        >{arguments, params}
+        >{arguments, params}, period(getAveragingPeriod(params))
     {}
 
     AggregationFunctionExpMovingAverage()
@@ -69,7 +90,7 @@ public:
             this->data(place).initialized = true;
         } else {
             Int64 delta = ts - this->data(place).ts;
-            auto alpha = 1 - std::exp(-delta);
+            auto alpha = 1 - std::exp(-delta / period);
             this->data(place).avg = alpha * value + (1 - alpha) * this->data(place).avg;
         } 
         this->data(place).ts = ts;
